cff9c2db5ce667c4a773327893b6219bb99c41ec
chandrahaskoneru <123540688+chandrahaskoneru@users.noreply.github.com>
2023-11-24 14:58:24 +0530

    Update example.tf

--- example/default/example.tf
+++ example/default/example.tf
@@ -15,7 +15,7 @@ locals {
 ## A VPC is a virtual network that closely resembles a traditional network that you'd operate in your own data center.
 ##=====================================================================================
 module "vpc" {
-  source      = "git::https://github.com/opz0/terraform-aws-vpc.git?ref=v1.0.0"
+  source      = "git::https://github.com/cypik/terraform-aws-vpc.git?ref=v1.0.0"
   name        = "app"
   environment = local.environment
   label_order = local.label_order
@@ -26,7 +26,7 @@ module "vpc" {
 ## A subnet is a range of IP addresses in your VPC.
 ##========================================================================
 module "public_subnets" {
-  source             = "git::https://github.com/opz0/terraform-aws-subnet.git?ref=v1.0.0"
+  source             = "git::https://github.com/cypik/terraform-aws-subnet.git?ref=v1.0.0"
   name               = "public-subnet"
   environment        = local.environment
   label_order        = local.label_order
@@ -39,7 +39,7 @@ module "public_subnets" {
 }
 
 module "iam-role" {
-  source             = "git::https://github.com/opz0/terraform-aws-iam-role.git?ref=v1.0.0"
+  source             = "git::https://github.com/cypik/terraform-aws-iam-role.git?ref=v1.0.0"
   name               = "iam-role"
   environment        = local.environment
   label_order        = local.label_order
