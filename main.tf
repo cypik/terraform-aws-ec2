853e2a1cdf2674b051d79b481ea051ae558263e4
chandrahaskoneru <123540688+chandrahaskoneru@users.noreply.github.com>
2023-11-24 14:56:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 ## Labels module callled that will be used for naming and tags.
 ##==================================================================================
 module "labels" {
-  source      = "git::https://github.com/opz0/terraform-aws-labels.git?ref=v1.0.0"
+  source      = "git::https://github.com/cypik/terraform-aws-labels.git?ref=v1.0.0"
   name        = var.name
   repository  = var.repository
   environment = var.environment
